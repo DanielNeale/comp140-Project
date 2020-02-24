9efc3a8de251ca1964a6ca4a5babb92b9c4abe30
dn230726 <dn230726@falmouth.ac.uk>
2020-02-24 14:54:44 +0000

    Rails switch with a key press

---
+++ Unity Project/COMP140 Game/Assets/Scripts/RailController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] rails;
+    
+    private bool railOneActive;
+    private bool railTwoActive;
+    private bool railThreeActive;
+    private bool railFourActive;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            railOneActive = !railOneActive;
+            transformRail(railOneActive, 0, 1);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            railTwoActive = !railTwoActive;
+            transformRail(railTwoActive, 1, -1);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            railThreeActive = !railThreeActive;
+            transformRail(railThreeActive, 2, 1);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            railFourActive = !railFourActive;
+            transformRail(railFourActive, 3, -1);
+        }
+    }
+
+    private void transformRail(bool active, int rail, int direction)
+    {
+        if (active)
+        {
+            rails[rail].Rotate(Vector3.up, 15 * direction);
+        }
+
+        if (!active)
+        {
+            rails[rail].Rotate(Vector3.up, -15 * direction);
+        }
+    }
+}
