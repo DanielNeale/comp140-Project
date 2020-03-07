12caea699811072612004659f32bf970c13a0bef
DanielNeale <danielneale01@gmail.com>
2020-03-07 18:04:20 +0000

    Started new method of train movement where trains are two separate parts which follow each other

--- Unity Project/COMP140 Game/Assets/Scripts/TrainController.cs
+++ Unity Project/COMP140 Game/Assets/Scripts/TrainController.cs
@@ -5,33 +5,42 @@ using UnityEngine;
 public class TrainController : MonoBehaviour
 {
     [SerializeField]
-    private int speed = 0;
+    private float speed = 0;
+    [SerializeField]
+    private Transform otherTrain = null;
 
-    void Start()
-    {
-        GetComponent<Rigidbody>().velocity = transform.forward * speed;
-    }
+    private Vector3 start;
+    private Vector3 end;
+    private float newSpeed;
+    private float time;
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (start != new Vector3(0,0,0))
+        {
+            transform.position = Vector3.Lerp(start, end, time * newSpeed);
+
+            transform.LookAt(otherTrain);
+
+            time += Time.fixedTime;
+        }      
     }
 
     private void OnTriggerEnter(Collider node)
     {
+
         Transform rail = node.transform.parent;
-        Transform targetNode = null;
 
         for (int i = 0; i < rail.childCount; i++)
         {
             if (rail.GetChild(i) != node.transform)
             {
-                targetNode = rail.GetChild(i);
-                Debug.Log(targetNode);
+                start = node.transform.position;
+                end = rail.GetChild(i).position;
             }
         }
 
-        transform.LookAt(targetNode.position);
-        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+        time = 0;
+        newSpeed = speed / Vector3.Distance(start, end);
     }
 }
