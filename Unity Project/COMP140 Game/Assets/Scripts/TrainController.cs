a121b4840f2cd1c785b54d4ab7d19c27d3ff4b87
DanielNeale <danielneale01@gmail.com>
2020-03-08 18:06:17 +0000

    Trains now follow and bend with the track

--- Unity Project/COMP140 Game/Assets/Scripts/TrainController.cs
+++ Unity Project/COMP140 Game/Assets/Scripts/TrainController.cs
@@ -14,6 +14,39 @@ public class TrainController : MonoBehaviour
     private float newSpeed;
     private float time;
 
+    private Transform node;
+
+    void Update()
+    {
+        if (node != null)
+        {
+            int nodeX = Mathf.RoundToInt(node.transform.position.x * 10);
+            int nodeZ = Mathf.RoundToInt(node.transform.position.z * 10);
+            int trainX = Mathf.RoundToInt(transform.position.x * 10);
+            int trainZ = Mathf.RoundToInt(transform.position.z * 10);
+
+            Debug.Log((nodeX, trainX, nodeZ, trainZ));
+
+            if (nodeX == trainX && nodeZ == trainZ)
+            {
+                Transform rail = node.transform.parent;
+
+                for (int i = 0; i < rail.childCount; i++)
+                {
+                    if (rail.GetChild(i) != node.transform)
+                    {
+                        start = node.transform.position;
+                        end = rail.GetChild(i).position;
+                    }
+                }
+
+                time = 0;
+                newSpeed = speed / Vector3.Distance(start, end);
+                node = null;
+            }
+        }
+    }
+
     void FixedUpdate()
     {
         if (start != new Vector3(0,0,0))
@@ -26,21 +59,8 @@ public class TrainController : MonoBehaviour
         }      
     }
 
-    private void OnTriggerEnter(Collider node)
+    private void OnTriggerEnter(Collider collision)
     {
-
-        Transform rail = node.transform.parent;
-
-        for (int i = 0; i < rail.childCount; i++)
-        {
-            if (rail.GetChild(i) != node.transform)
-            {
-                start = node.transform.position;
-                end = rail.GetChild(i).position;
-            }
-        }
-
-        time = 0;
-        newSpeed = speed / Vector3.Distance(start, end);
+        node = collision.transform;                      
     }
 }
