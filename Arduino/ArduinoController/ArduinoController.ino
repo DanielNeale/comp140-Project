1e6ca77b3e99e3af4d3a7bfe9c83eda08a9f2b18
Daniel Neale <56132179+DanielNeale@users.noreply.github.com>
2020-03-27 01:48:39 +0000

    Arduino communicates with unity

--- Arduino/ArduinoController/ArduinoController.ino
+++ Arduino/ArduinoController/ArduinoController.ino
@@ -1,30 +1,150 @@
-int voltage = 0;
-bool on = false;
+int voltageA = 0;
+int voltageB = 0;
+int voltageC = 0;
+int voltageD = 0;
+int voltageSwitch = 0;
+
+bool onA = false;
+bool onB = false;
+bool onC = false;
+bool onD = false;
+
+char input;
 
 void setup() 
 {
   Serial.begin(9600);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
 }
 
 void loop() 
 {
-  voltage = analogRead(A0);
+  voltageA = analogRead(A0);
+  voltageB = analogRead(A1);
+  voltageC = analogRead(A2);
+  voltageD = analogRead(A3);
+  voltageSwitch = analogRead(A5);
 
-  if (voltage > 1000)
+  if (Serial.available() > 0) 
   {
-    on = true;
+    input = Serial.read();
+    
+    if (input = "R")
+    {
+      Serial.print(onA);
+      Serial.print(onB);
+      Serial.print(onC);
+      Serial.println(onD);
+    } 
+  }
+
+  
+  if (voltageA > 1000)
+  {
+    if (voltageSwitch > 1000)
+    {
+      onA = true;     
+    }
+
+    else
+    {
+      onA = false;     
+    }   
+  }
+
+  else if (voltageB > 1000)
+  {
+    if (voltageSwitch > 1000)
+    {
+      onB = true;
+    }
+
+    else
+    {
+      onB = false;
+    }   
+  }
+
+  else if (voltageC > 1000)
+  {
+    if (voltageSwitch > 1000)
+    {
+      onC = true;
+    }
+
+    else
+    {
+      onC = false;
+    }   
+  }
+
+  else if (voltageD > 1000)
+  {
+    if (voltageSwitch > 1000)
+    {
+      onD = true;
+    }
+
+    else
+    {
+      onD = false;
+    }
+  }
+
+  if (onA)
+  {
     digitalWrite(12, LOW);
     digitalWrite(13, HIGH);
   }
 
   else
   {
-    on = false;
-    digitalWrite(13, LOW);
     digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
   }
-  
-  Serial.println(on);
+
+  if (onB)
+  {
+    digitalWrite(10, LOW);
+    digitalWrite(11, HIGH);
+  }
+
+  else
+  {
+    digitalWrite(10, HIGH);
+    digitalWrite(11, LOW);
+  }
+
+  if (onC)
+  {
+    digitalWrite(8, LOW);
+    digitalWrite(9, HIGH);
+  }
+
+  else
+  {
+    digitalWrite(8, HIGH);
+    digitalWrite(9, LOW);
+  }
+
+  if (onD)
+  {
+    digitalWrite(6, LOW);
+    digitalWrite(7, HIGH);
+  }
+
+  else
+  {
+    digitalWrite(6, HIGH);
+    digitalWrite(7, LOW);
+  }
+
+  delay(50);
 }
